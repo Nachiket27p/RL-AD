f09cc903e74a99a7ebf1b2ae86ec7f7af894bfe8
Nachiket27p <nnpatel5@wolftech.ad.ncsu.edu>
2021-03-14 17:22:54 -0400

    updated cuda path export

---
+++ airsim/scripts/cudaPathExport.bat
@@ -0,0 +1,5 @@
+SET PATH=C:\Users\nnpatel5\Document\csc720\CUDA\v11.0\bin;%PATH%
+
+SET PATH=C:\Users\nnpatel5\Document\csc720\CUDA\v11.0\extras\CUPTI\lib64;%PATH%
+
+SET PATH=C:\Users\nnpatel5\Document\csc720\CUDA\v11.0\include;%PATH%
